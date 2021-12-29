e55844e5990db8f77ce01c1ac22043b609c11be3
Harsha-S2604 <harsha91148@outlook.com>
2021-12-29 13:42:58 +0000

    first commit

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, Welcome to the AWS IAM Command line interface :)")
+	fmt.Println("Please select a operation to perform")
+	fmt.Println("========================================================")
+	fmt.Println("1. Create a group\n2. Delete a group\n3. Create a user\n4. Delete a user")
+}
