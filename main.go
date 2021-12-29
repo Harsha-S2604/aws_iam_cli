13a44b241849d84aecc8f16dddf7a77ab4031122
Harsha-S2604 <harsha91148@outlook.com>
2021-12-29 14:28:08 +0000

    client for iam

--- main.go
+++ main.go
@@ -2,19 +2,32 @@ package main
 
 import (
 	"fmt"
+	"context"
+	"log"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/iam"
 )
 
 func main() {
 
 	// welcome note and description
 	fmt.Println("Hello, Welcome to the AWS IAM Command line interface :)")
-	fmt.Println("Please select a operation to perform")
+
+	// reads the access key from ~/.aws/config and ~/.aws/credentials
+	cfg, cfgErr := config.LoadDefaultConfig(context.TODO())
+	if cfgErr != nil {
+		log.Fatal("configuration error. Please configure the AWS. For more information visit https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html")
+	}
+
+	iamClient := iam.NewFromConfig(cfg)
+	
+	fmt.Println("Please select any option to perform operation")
 	fmt.Println("========================================================")
 	fmt.Println("1. Create a group\n2. Delete a group\n3. Create a user\n4. Delete a user")
 	
 	// get the input from user using Scanf function
 	var option int
-	fmt.Printf("Enter the number: ")
+	fmt.Printf("Enter the option: ")
 	fmt.Scanf("%d", &option)
 	
 	// call function based on the user input
@@ -27,28 +40,28 @@ func main() {
 	*/
 	switch(option) {
 		case 1:
-		    CreateGroup()
+		    CreateGroup(iamClient)
 		case 2:
-		    DeleteGroup()
+		    DeleteGroup(iamClient)
 		case 3:
-		    CreateUser()
+		    CreateUser(iamClient)
 		case 4:
-		    DeleteUser()
+		    DeleteUser(iamClient)
 	}
 }
 
-func CreateGroup() {
+func CreateGroup(iamClient *iam.Client) {
 	fmt.Println("Create group")
 }
 
-func DeleteGroup() {
+func DeleteGroup(iamClient *iam.Client) {
 	fmt.Println("Delete group")
 }
 
-func CreateUser() {
+func CreateUser(iamClient *iam.Client) {
 	fmt.Println("Create user")
 }
 
-func DeleteUser() {
+func DeleteUser(iamClient *iam.Client) {
 	fmt.Println("Delete user")
 }
