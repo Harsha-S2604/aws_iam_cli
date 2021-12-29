f356f6f704a011e1c687ececeeadda7531981ebd
Harsha-S2604 <harsha91148@outlook.com>
2021-12-29 13:53:24 +0000

    get input from the user

--- main.go
+++ main.go
@@ -9,4 +9,10 @@ func main() {
 	fmt.Println("Please select a operation to perform")
 	fmt.Println("========================================================")
 	fmt.Println("1. Create a group\n2. Delete a group\n3. Create a user\n4. Delete a user")
+	
+	// get the input from user using Scanf function
+	var option int
+	fmt.Printf("Enter the number: ")
+	fmt.Scanf("%d", &option)
+	fmt.Println(option)
 }
